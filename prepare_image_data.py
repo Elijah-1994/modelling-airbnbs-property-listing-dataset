1b2e22acb6e4c6d7749df8939f4b038cbe825ef1
Elijah-1994 <elijah.salami94@gmail.com>
2022-12-13 14:29:42 +0000

    Fifth commit - Uploading image data from aws

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,9 +1,26 @@
 #%%
-import boto3 
-import s3fs
-s3_file = s3fs.S3FileSystem()
-local_path = "airbnb-property-listings/images"
-s3_path = "airbnb-property-listings/airbnb-property-listings/images"
-s3_file.put(local_path, s3_path, recursive=True) 
-#%%
+import boto3
+import os 
+from PIL import Image
 
+def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucketName) 
+    for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+        if not os.path.exists(os.path.dirname(obj.key)):
+            os.makedirs(os.path.dirname(obj.key))
+        bucket.download_file(obj.key, obj.key) # save to same path
+        
+def resize_images(df_copy):
+    print('hello')
+        
+filepath = "geeksforgeeks.png"
+img = Image.open(filepath)
+width = img.width
+height = img.height
+print("The height of the image is: ", height)
+print("The width of the image is: ", width)
+
+
+
+#downloadDirectoryFroms3('airbnb-property-listings','images/')
