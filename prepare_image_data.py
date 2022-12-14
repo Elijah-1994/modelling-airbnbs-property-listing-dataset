4c173e043653c93ed69fbb7350adf05ad42d5bf1
Elijah-1994 <elijah.salami94@gmail.com>
2022-12-14 15:07:51 +0000

    Third commit - Uploading Milestone 1 script.

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,26 +1,59 @@
 #%%
-import boto3
-import os 
+import os.path
 from PIL import Image
-
+import boto3 
+from time import sleep
+#%%
 def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(bucketName) 
     for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
         if not os.path.exists(os.path.dirname(obj.key)):
             os.makedirs(os.path.dirname(obj.key))
-        bucket.download_file(obj.key, obj.key) # save to same path
-        
-def resize_images(df_copy):
-    print('hello')
-        
-filepath = "geeksforgeeks.png"
-img = Image.open(filepath)
-width = img.width
-height = img.height
-print("The height of the image is: ", height)
-print("The width of the image is: ", width)
+        bucket.download_file(obj.key, obj.key)
 
+def create_directory():
+    images_directory ='processed_images' 
+    parent_directory = os.getcwd()
+    path = os.path.join(parent_directory, images_directory) 
+    return path
 
+def calculate_smallest_image_height(path):
+    image_height = []
+    for dirpath, _, filenames in os.walk(path):
+        for path_image in filenames:
+            image = os.path.join(dirpath, path_image)
+            with Image.open(image) as img:
+                height = img.height
+                image_height.append(height)
+    minimum_height = min(image_height)
+    return minimum_height
 
-#downloadDirectoryFroms3('airbnb-property-listings','images/')
+def delete_image_by_mode(mode, file_path, img):
+    if img.mode != mode:
+            sleep(5)
+            os.remove(file_path, dir_fd=None)
+
+def resize_images(path):
+    for dirpath, _, filenames in os.walk(path):
+        for path_image in filenames:
+            base_height = calculate_smallest_image_height(path)
+            image = os.path.join(dirpath, path_image)
+            with Image.open(image) as img:
+                width = img.width
+                height = img.height
+                aspect_ratio = width / height
+                new_width = int(base_height*aspect_ratio)
+                img = img.resize((base_height,new_width), Image.Resampling.LANCZOS)
+                image_path_and_name = os.path.split(image) 
+                image_name_and_ext = os.path.splitext(image_path_and_name[1]) 
+                name = image_name_and_ext[0] + '.png'
+                file_path = os.path.join(create_directory(), name)
+                img.save(file_path)
+                delete_image_by_mode('RGB', file_path, img)
+
+if __name__ == '__main__':
+    #downloadDirectoryFroms3('airbnb-property-listings', 'images')
+    resize_images('images')
+             
+
--- upload_image_data.py
+++
@@ -1,9 +0,0 @@
-#%%
-import boto3 
-import s3fs
-s3_file = s3fs.S3FileSystem()
-local_path = "airbnb-property-listings/images"
-s3_path = "airbnb-property-listings/airbnb-property-listings/images"
-s3_file.put(local_path, s3_path, recursive=True) 
-#%%
-
