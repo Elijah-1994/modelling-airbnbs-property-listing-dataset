47c42adb3678b8b9fa08a6b0ee57fe26062767c3
Elijah-1994 <elijah.salami94@gmail.com>
2022-12-27 15:05:46 +0000

    Seventh commit - Uploading modelling.py script.

--- modelling.py
+++ modelling.py
@@ -1,13 +1,13 @@
 #%%
 from tabular_data import load_airbnb
-from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import scale
-from sklearn.linear_model import SGDRegressor
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 import joblib
 import json
 import numpy as np
@@ -15,7 +15,7 @@ import pandas as pd
 import json
 #%%
 def evaluate_all_models(X_train,X_test,y_train,y_test):
-    models = [SGDRegressor(),DecisionTreeRegressor(),]
+    models = [SGDRegressor(),DecisionTreeRegressor(),RandomForestRegressor(),GradientBoostingRegressor()]
     for model in models:
         model_name = str(model)
         best_parameters = tune_regression_model_hyperparameters(model,model_name,X_train, y_train)
@@ -76,6 +76,55 @@ def tune_regression_model_hyperparameters(model,model_name,X_train, y_train):
         clf.best_params_
         return clf.best_params_
 
+    elif model_name == 'RandomForestRegressor()':
+        params = {
+            "n_estimators" : [5,20,50,100],
+            "max_features":[1.0,"log2","sqrt",None],
+            "max_leaf_nodes":[None,10,20,30,40,50,60,70,80,90], 
+            "max_depth" : [1,3,5,7,9,11,12],
+            "min_weight_fraction_leaf":[0.1,0.2,0.3,0.4],
+            "min_samples_split" : [2, 6, 10],
+            "min_samples_leaf":[1,2,3,4,5,6,7,8,9,10],
+            "bootstrap" : [True, False]
+    
+                                                              }
+        clf = GridSearchCV(
+        estimator=model,
+        scoring='neg_mean_squared_error',
+        param_grid=params,
+        cv=5,
+        n_jobs=1,
+        verbose=1
+                    )
+        clf.fit(X_train, y_train)
+        clf.best_params_
+        return clf.best_params_
+    
+    elif model_name == 'GradientBoostingRegressor()':
+        params = {
+            "learning_rate" : [0.1,1.0,3.0,6.0,9.0,12.0],
+            "n_estimators" : [5,20,50,100],
+            "subsample": [0.1,0.2,0.3,0.4,0.5,0.5,0.6,0.7,0.8,0.9,1.0],
+            "min_samples_split" : [2, 6, 10],
+            "min_samples_leaf":[1,2,3,4,5,6,7,8,9,10],
+            "min_weight_fraction_leaf":[0.1,0.2,0.3,0.4],
+            "max_depth" : [1,3,5,7,9,11,12],
+            "max_features":[1.0,"log2","sqrt",None],
+            "max_leaf_nodes":[None,10,20,30,40,50,60,70,80,90],
+            "alpha":[0.1,0.2,0.3,0.4,0.5,0.5,0.6,0.7,0.8,0.9,1.0],
+            'tol':[0.0001,0.0002,0.0003]
+                                                              }
+        clf = GridSearchCV(
+        estimator=model,
+        scoring='neg_mean_squared_error',
+        param_grid=params,
+        cv=5,
+        n_jobs=1,
+        verbose=1
+                    )
+        clf.fit(X_train, y_train)
+        clf.best_params_
+        return clf.best_params_
 
 def save_model(model,model_name,best_parameters,performance_metrics):
     if model_name == 'SGDRegressor()':
@@ -89,7 +138,19 @@ def save_model(model,model_name,best_parameters,performance_metrics):
         with open("models/hyperparameters/DecisionTreeRegressor/hyperparameters.json", mode="w", encoding= "utf-8") as file:
             file.write(json.dumps((best_parameters), default=str))   
         with open("models/performance_metrics/DecisionTreeRegressor/performance_metrics.json", mode="w", encoding= "utf-8") as file:
+            file.write(json.dumps((performance_metrics), default=str)) 
+    elif model_name == 'RandomForestRegressor()':
+        joblib.dump(model, "models/regression/RandomForestRegressor/model.joblib")
+        with open("models/hyperparameters/RandomForestRegressor/hyperparameters.json", mode="w", encoding= "utf-8") as file:
+            file.write(json.dumps((best_parameters), default=str))   
+        with open("models/performance_metrics/RandomForestRegressor/performance_metrics.json", mode="w", encoding= "utf-8") as file:
             file.write(json.dumps((performance_metrics), default=str))  
+    elif model_name == 'DecisionTreeRegressor()':
+        joblib.dump(model, "models/regression/DecisionTreeRegressor/model.joblib")
+        with open("models/hyperparameters/DecisionTreeRegressor/hyperparameters.json", mode="w", encoding= "utf-8") as file:
+            file.write(json.dumps((best_parameters), default=str))   
+        with open("models/performance_metrics/DecisionTreeRegressor/performance_metrics.json", mode="w", encoding= "utf-8") as file:
+            file.write(json.dumps((performance_metrics), default=str))   
 
 if __name__ == '__main__':
     np.random.seed(10)
@@ -101,7 +162,7 @@ if __name__ == '__main__':
     y = rng.randn(n_samples)
     X_train, X_test,y_train, y_test= train_test_split(X, y, test_size=0.3)
     evaluate_all_models(X_test,X_train,y_test, y_train)
-   
+ 
 
 
 
